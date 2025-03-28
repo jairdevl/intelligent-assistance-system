1439dfc0f2d54b2c104f10ea8977d5ff17c68f6c
Jaireua <jairenriquez30@gmail.com>
2025-03-28 16:54:13 -0500

    creation signup app

--- accounts/models.py
+++ accounts/models.py
@@ -2,8 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
 class UserImages(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    number_identification = models.CharField(max_length=15, unique=True)
     image = models.ImageField(upload_to='user_faces/')
 
     def __str__(self):
--- accounts/views.py
+++ accounts/views.py
@@ -11,15 +11,24 @@ def signup(request):
     if request.method == 'POST':
         username = request.POST['username']
         face_image_data = request.POST['face_image']
+        number_identification = request.POST['number_identification']
+
+        # Check if user already exists
+        if UserImages.objects.filter(number_identification=number_identification).exists():
+            return JsonResponse({'status': 'error', 'message': 'User already exists!'})
 
         # Convert base64 image data to file
         face_image_data = face_image_data.split(',')[1]
-        face_image = ContentFile(base64.b64decode(face_image_data), name=f'{username}.jpg')
+        face_image = ContentFile(base64.b64decode(face_image_data), name=f'{number_identification}.jpg')
 
         # Save the user and face image in the database
         user = User(username=username)
         user.save()
-        user_image = UserImages.objects.create(user=user, image=face_image)
+        UserImages.objects.create(
+            user=user, 
+            image=face_image,
+            number_identification=number_identification
+        )
 
         return JsonResponse({'status': 'success', 'message': 'User created successfully!'})
 
