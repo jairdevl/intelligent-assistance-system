2c917d5e8546b42bf36f3203192fb0caf774e63d
Jaireua <jairenriquez30@gmail.com>
2025-04-01 19:58:57 -0500

    creating backed signin

--- accounts/views.py
+++ accounts/views.py
@@ -37,5 +37,40 @@ def signup(request):
     return render(request, 'signup.html')
 
 def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        face_image_data = request.POST['face_image']
+
+        # Get the user by username
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'error', 'message': 'User does not exist!'})
+        
+        # Convert base64 image data to file
+        face_image_data = face_image_data.split(','[1])
+        uploaded_image = ContentFile(base64.b64decode(face_image_data), name=f'{username}.jpg')
+
+        # Compare the uploaded face image with the stored face image
+        uploaded_face_image = face_recognition.load_image_file(uploaded_image)
+        uploaded_face_encoding = face_recognition.face_encodings(uploaded_face_image)
+
+        if uploaded_face_encoding:
+            uploaded_face_encoding = uploaded_face_encoding[0]
+            user_image = UserImages.objects.get(user=user).first()
+            stored_face_image = face_recognition.load_image_file(user_image.face_image.path)
+            stored_face_encoding = face_recognition.face_encodings(stored_face_image)[0]
+
+            print(stored_face_image, stored_face_encoding)
+            # Compare the faces
+            match = face_recognition.compare_faces([stored_face_encoding], uploaded_face_encoding)
+
+            if match[0]:
+                return JsonResponse({'status': 'success', 'message': 'Login successful!', redirect: '/signup'})
+            else:
+                return JsonResponse({'status': 'error', 'message': 'Face does not match!'})
+        
+        return JsonResponse({'status': 'error', 'message': 'Face not detected in the image!'})
+
     return render(request, 'signin.html')
                     
